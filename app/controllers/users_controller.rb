fc165a9fe2b2a5aba09c682be04a3b399632981d
Pupi <dreamjl@live.cn>
2015-07-13 23:21:36 +0800

    add create user action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,20 @@ class UsersController < ApplicationController
   def new
 
   end
-  def create
-    render plain: params[:user].inspect
-  end
-  def index
 
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+    redirect_to @user
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :telephone)
   end
 end
